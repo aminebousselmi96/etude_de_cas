79efc238229eede5116feacdf3b724ee9258942a
Pierre Desmarchelier <pierre.desmarchelier@2019.icam.fr>
2019-12-07 18:09:43 +0100

    Adding of : RealEstate in database +  modification of ma schema

--- app.py
+++ app.py
@@ -24,22 +24,48 @@ class User(db.Model):
     last_name = db.Column(db.String(50), nullable=False)
     first_name = db.Column(db.String(20))
     birth_date = db.Column(db.String(20))
+    real_estates = db.relationship('RealEstate', backref='owner', lazy='dynamic')
 
-    def __init__(self, last_name, first_name, birth_date):
+    def __init__(self,last_name,first_name,birth_date):
         self.last_name = last_name
         self.first_name = first_name
         self.birth_date = birth_date
 
+    def __repr__(self):
+        return '<User {} {}>'.format(self.last_name, self.first_name)
+    
+
+# Real Estate ClassModel
+class RealEstate(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), unique=True, nullable=False)
+    description = db.Column(db.String(100))
+    kind = db.Column(db.String(50))
+    town = db.Column(db.String(50), nullable=False)
+    nb_room = db.Column(db.Integer)
+    room_description = db.Column(db.String(100))
+    owner_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    
+    def __repr__(self):
+        return '<RealEstate {}>'.format(self.name)
+
  
 # User Schema
-class UserSchema(ma.Schema):
+class UserSchema(ma.ModelSchema):
     class Meta:
-        fields = ('id','last_name', 'first_name', 'birth_date')
+        model = User
+
+# RealEstate
+class RealEstateSchema(ma.ModelSchema):
+    class Meta:
+        model = RealEstate
 
 
 # Init schema
 user_schema = UserSchema()
 users_schema = UserSchema(many=True)
+real_estate_schema = RealEstateSchema()
+real_estates_schema = RealEstateSchema(many=True)
 
 
 @app.route('/', methods=['GET'])
@@ -86,4 +112,6 @@ def update_user(id):
 
 # Run Server
 if __name__ == '__main__':
+    db.create_all()
     app.run(debug=True)
+    
